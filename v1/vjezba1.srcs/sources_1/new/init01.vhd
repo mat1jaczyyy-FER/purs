d175a84ed6bc5430ff74ef83cde9cc03f850d54a
mat1jaczyyy <mat1jaczyyy@gmail.com>
2022-11-08 16:05:09 +0100

    3.10

--- v1/vjezba1.srcs/sources_1/new/init01.vhd
+++ v1/vjezba1.srcs/sources_1/new/init01.vhd
@@ -1,4 +1,4 @@
 package init01 is
     -- 3 for sim, 26 for ZedBoard
-    constant CLK_divider_width: integer range 2 to 26 := 3;
+    constant CLK_divider_width: integer range 2 to 26 := 26;
 end package init01;
